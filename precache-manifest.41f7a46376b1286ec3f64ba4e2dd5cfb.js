a7fc2403791e1dfe66b55ed835742acd80c05896
Eva Dressel <evapiscola@gmail.com>
2020-07-16 14:12:25 +0200

    Updates

---
+++ precache-manifest.41f7a46376b1286ec3f64ba4e2dd5cfb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0f28af9dd72093c9a373881baa8c8c2",
+    "url": "/project/index.html"
+  },
+  {
+    "revision": "ca80201fb81b528be9dc",
+    "url": "/project/static/css/main.3c65908e.chunk.css"
+  },
+  {
+    "revision": "fea92983a7d311fbb6d6",
+    "url": "/project/static/js/2.d57e10e9.chunk.js"
+  },
+  {
+    "revision": "ecceb24af704d5a9ac879d57a1aaf3d0",
+    "url": "/project/static/js/2.d57e10e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca80201fb81b528be9dc",
+    "url": "/project/static/js/main.b6c7a909.chunk.js"
+  },
+  {
+    "revision": "e41c2154ffae2dd7167b",
+    "url": "/project/static/js/runtime-main.e1e797e8.js"
+  },
+  {
+    "revision": "b5c2c197ed676ac8882c0eecf3225dd1",
+    "url": "/project/static/media/Impact_Label.b5c2c197.ttf"
+  },
+  {
+    "revision": "c863f8028c2505f92540e0ba7c379002",
+    "url": "/project/static/media/PermanentMarker-Regular.c863f802.ttf"
+  },
+  {
+    "revision": "daf714537074674a4d119a7321ef57d9",
+    "url": "/project/static/media/legal-pad.daf71453.png"
+  }
+]);
